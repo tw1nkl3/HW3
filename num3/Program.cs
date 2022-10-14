02bfe415570a3d6b2d3559036adb5dfc4727c90b
Ivan <grabovivan@gmail.com>
2022-10-14 19:42:30 +0300

    num3

---
+++ num3/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = int.Parse(Console.ReadLine());
+List<int> array = new List<int>();
+
+for (int i = 1; i <= num; i++)
+{ 
+    array.Add(i*i*i);
+}
+
+Console.Write(String.Join(", ", array));
