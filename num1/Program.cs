59e71c1562098e5a6aeab0a3907ab0c08be77d3f
Ivan <grabovivan@gmail.com>
2022-10-14 19:28:50 +0300

    num1

---
+++ num1/Program.cs
@@ -0,0 +1,17 @@
+﻿string num = Console.ReadLine();
+
+if (num.Length == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
